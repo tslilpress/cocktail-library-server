46b9e9f997ecc308512105c1aade3bf599d21980
tslilpress <tslilpress@gmail.com>
2020-09-22 19:32:06 -0400

    Added ingredients to update curl script

--- curl-scripts/cocktails/update.sh
+++ curl-scripts/cocktails/update.sh
@@ -15,6 +15,7 @@ curl "${API}${URL_PATH}/${ID}" \
     "serveIn": "'"${IN}"'",
     "howToServe": "'"${HOW}"'",
     "garnish": "'"${GARNISH}"'",
+    "ingredients": "'"${INGRED}"'",
     "owner": "'"${OWNER}"'",
     "note": "'"${NOTE}"'"
   }
